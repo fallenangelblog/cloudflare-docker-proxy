c3c24c25df5cc5ba83d444818de47a3f78466513
FunnyBunny <34016559+fallenangelblog@users.noreply.github.com>
2024-07-12 16:18:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  "docker.mydomain.com": "https://registry-1.docker.io",
-  "quay.mydomain.com": "https://quay.io",
-  "gcr.mydomain.com": "https://gcr.io",
-  "k8s-gcr.mydomain.com": "https://k8s.gcr.io",
-  "k8s.mydomain.com": "https://registry.k8s.io",
-  "ghcr.mydomain.com": "https://ghcr.io",
-  "cloudsmith.mydomain.com": "https://docker.cloudsmith.io",
+  "docker.01lb.vip": "https://registry-1.docker.io",
+  "quay.01lb.vip": "https://quay.io",
+  "gcr.01lb.vip": "https://gcr.io",
+  "k8s-gcr.01lb.vip": "https://k8s.gcr.io",
+  "k8s.01lb.vip": "https://registry.k8s.io",
+  "ghcr.01lb.vip": "https://ghcr.io",
+  "cloudsmith.01lb.vip": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
